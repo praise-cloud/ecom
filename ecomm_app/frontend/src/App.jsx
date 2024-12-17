3a9dd8762c549a0f8f636278a0f980af6b158e6b
Praise-cloud <georgewillpraise1@example.com>
2024-12-17 11:18:07 +0100

    added route to the App.jsx file

--- ecomm_app/frontend/src/App.jsx
+++ ecomm_app/frontend/src/App.jsx
@@ -1,11 +1,21 @@
 // import React from 'react'
-import { Routes } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Collection from "./pages/Collection"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Routes>
-
+        <Route path="/" element={<Home/>} />
+        <Route path="/colection" element={<Collection/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/product/:productId" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
       </Routes>
 
     </div>
