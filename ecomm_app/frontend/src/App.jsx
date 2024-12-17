ef4543e709b4a6ef19609cc526166a2e25825185
Praise-cloud <georgewillpraise1@example.com>
2024-12-17 02:55:44 +0100

    made files under pages folder

--- ecomm_app/frontend/src/App.jsx
+++ ecomm_app/frontend/src/App.jsx
@@ -1,8 +1,14 @@
 // import React from 'react'
+import { Routes } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+
+      </Routes>
+
+    </div>
   )
 }
 
